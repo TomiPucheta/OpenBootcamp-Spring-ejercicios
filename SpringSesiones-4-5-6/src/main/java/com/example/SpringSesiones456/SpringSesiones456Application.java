a434ae6fc56ef3c0c221078368d9f1bc255db608
Tomás Pucheta <pucheta.tomas01@gmail.com>
2022-07-18 15:29:12 -0300

    primer commit

---
+++ SpringSesiones-4-5-6/src/main/java/com/example/SpringSesiones456/SpringSesiones456Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringSesiones456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSesiones456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSesiones456Application.class, args);
+	}
+
+}
