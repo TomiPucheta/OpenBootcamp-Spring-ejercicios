7622e51e315376331a44e1f762d8cfaef4d0207a
Tomás Pucheta <pucheta.tomas01@gmail.com>
2022-07-18 15:51:26 -0300

    ejercicio 1

---
+++ SpringSesiones-4-5-6/src/main/java/com/example/SpringSesiones456/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.SpringSesiones456;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    //Ejercicio 1
+
+    @GetMapping("/")
+    public String saludo() {
+        return "Hola Mundo!";
+    }
+}
